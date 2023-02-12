db3efc4f62e8d52771c8cedbaae06a30d690b5ed
Md. Masum Billah <md.masum123@gmail.com>
2023-02-13 00:46:37 +0600

    Figure 2-5. Our PizzaPlace’s Menu

---
+++ PizzaPlace/Shared/ShoppingBasket.cs
@@ -0,0 +1,12 @@
+﻿namespace PizzaPlace.Shared
+{
+    public class ShoppingBasket
+    {
+        public Customer Customer { get; set; } = new Customer();
+        public List<int> Orders { get; set; } = new List<int>();
+        public bool HasPaid { get; set; }
+
+        public void Add(int pizzaId)=> Orders.Add(pizzaId);
+
+    }
+}
---
+++ PizzaPlace/Shared/State.cs
@@ -0,0 +1,9 @@
+﻿namespace PizzaPlace.Shared
+{
+    public class State
+    {
+        public Menu Menu { get; } = new Menu();
+        public ShoppingBasket Basket { get; } = new ShoppingBasket();
+        public UI UI { get; set; } = new UI();
+    }
+}
