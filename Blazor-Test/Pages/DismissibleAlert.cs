3e3b179d31e057a4aba0a29088c8550110e0ec70
Md. Masum Billah <md.masum123@gmail.com>
2023-02-15 01:42:44 +0600

    Listing 3-7. Using Dismissible

---
+++ Blazor-Test/Pages/DismissibleAlert.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Blazor_Test.Pages
+{
+    public partial class DismissibleAlert
+    {
+        [Parameter]
+        public bool Show { get; set; }
+        [Parameter]
+        public RenderFragment ChildContent { get; set; } = default!;
+        public void Dismiss()
+        => Show = false;
+    }
+}
