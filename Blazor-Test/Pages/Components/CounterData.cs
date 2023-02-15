66adf77cfaa7ee6f8c369995934cfd8ef3752a79
Md. Masum Billah <md.masum123@gmail.com>
2023-02-16 02:14:02 +0600

    Listing 3-21. Receiving the Cascading Value

---
+++ Blazor-Test/Pages/Components/CounterData.cs
@@ -0,0 +1,20 @@
+﻿namespace Blazor_Test.Pages.Components
+{
+    public class CounterData
+    {
+        private int count;
+        public int Count
+        {
+            get => this.count;
+            set
+            {
+                if (value != count)
+                {
+                    this.count = value;
+                    CountChanged?.Invoke(this.count);
+                }
+            }
+        }
+        public Action<int>? CountChanged { get; set; }
+    }
+}
