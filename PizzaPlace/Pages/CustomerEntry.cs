65346fa6e6afa0cba5ea4d02e96ea0d25702d6dc
Md. Masum Billah <md.masum123@gmail.com>
2023-02-20 03:28:27 +0600

    Listing 3-54. Disabling the Submit Button

--- PizzaPlace/Pages/CustomerEntry.cs
+++ PizzaPlace/Pages/CustomerEntry.cs
@@ -15,5 +15,17 @@ namespace PizzaPlace.Pages
         public Customer Customer { get; set; } = default!;
         [Parameter]
         public EventCallback ValidSubmit { get; set; } = default!;
+
+
+        [Parameter]
+        public EventCallback<Customer> CustomerChanged { get; set; }
+        private void FieldChanged(string fieldName)
+        {
+            CustomerChanged.InvokeAsync(Customer);
+            isInvalid = !inputWatcher.Validate();
+        }
+
+        private InputWatcher inputWatcher = default!;
+        bool isInvalid = true;
     }
 }
