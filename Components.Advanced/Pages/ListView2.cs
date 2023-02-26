495d9e1acb12bf6679168782e35c783e68c076fe
Md. Masum Billah <md.masum123@gmail.com>
2023-02-27 03:01:27 +0600

    Listing 4-14. Using a Templated Component with ListTemplate

---
+++ Components.Advanced/Pages/ListView2.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Components.Advanced.Pages
+{
+    public partial class ListView2<TItem>
+    {
+        [Parameter]
+        public RenderFragment<RenderFragment>? ListTemplate
+        { get; set; }
+        [Parameter]
+        public RenderFragment<TItem> ItemTemplate
+        { get; set; } = default!;
+
+        [Parameter]
+        public IReadOnlyList<TItem> Items
+        { get; set; } = default!;
+    }
+}
