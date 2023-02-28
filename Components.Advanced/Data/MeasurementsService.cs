f902a5bc3972e1bbfdd40306b4c676559390be40
Md. Masum Billah <md.masum123@gmail.com>
2023-03-01 02:31:34 +0600

    Listing 4-26. Using the NonVirtualMeasurements Component

---
+++ Components.Advanced/Data/MeasurementsService.cs
@@ -0,0 +1,23 @@
+﻿namespace Components.Advanced.Data
+{
+    public class MeasurementsService
+    {
+        public ValueTask<List<Measurement>> GetMeasurements()
+        {
+            const int nrOfRows = 5000;
+            var result = new List<Measurement>();
+            var rnd = new Random();
+            for (int i = 0; i < nrOfRows; i += 1)
+            {
+                result.Add(new Measurement()
+                {
+                    Guid = Guid.NewGuid(),
+                    Min = rnd.Next(0, 100),
+                    Avg = rnd.Next(100, 300),
+                    Max = rnd.Next(300, 400),
+                });
+            }
+            return new ValueTask<List<Measurement>>(result);
+        }
+    }
+}
