656b3282301eaa9b15cf2f257998b2ba464a9445
Md. Masum Billah <md.masum123@gmail.com>
2023-03-02 01:43:32 +0600

    Listing 4-29. Using the ItemsProvider

--- Components.Advanced/Data/MeasurementsService.cs
+++ Components.Advanced/Data/MeasurementsService.cs
@@ -19,5 +19,28 @@
             }
             return new ValueTask<List<Measurement>>(result);
         }
+
+
+
+        public ValueTask<(List<Measurement>, int)> GetMeasurementsPage(int from, int count, CancellationToken cancellationToken)
+        {
+            const int maxMeasurements = 5000;
+            var result = new List<Measurement>();
+            var rnd = new Random();
+            count = Math.Max(0, Math.Min(count, maxMeasurements - from));
+            for (int i = 0; i < count; i += 1)
+            {
+                result.Add(new Measurement()
+                {
+                    Guid = Guid.NewGuid(),
+                    Min = rnd.Next(0, 100),
+                    Avg = rnd.Next(100, 300),
+                    Max = rnd.Next(300, 400),
+                });
+            }
+            return new ValueTask<(List<Measurement>, int)>((result, maxMeasurements));
+        }
+
+
     }
 }
