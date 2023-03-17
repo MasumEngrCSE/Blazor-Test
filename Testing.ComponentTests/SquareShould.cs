155e207652dc57a85e4912737194a840519f30d6
Md. Masum Billah <md.masum123@gmail.com>
2023-03-17 22:11:31 +0600

    Listing 8-4. Testing the Square Method

---
+++ Testing.ComponentTests/SquareShould.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Testing.Shared;
+using Xunit;
+
+namespace Testing.ComponentTests
+{
+    public class SquareShould
+    {
+        [Fact]
+        public void Return9For3()
+        {
+
+            // Arrange
+            var sut = new Utils();
+            // Act
+            var actual = sut.Square(3);
+            // Assert
+            Assert.Equal(expected: 9, actual: actual);
+
+        }
+    }
+}
---
+++ Testing.Shared/Utils.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testing.Shared
+{
+    public class Utils
+    {
+        public int Square(int i)
+        {
+            return i;
+        }
+    }
+}
