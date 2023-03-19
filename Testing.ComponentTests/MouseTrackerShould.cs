f71059b3464387f3c9c6f36c8b8181717a0854b9
Md. Masum Billah <md.masum123@gmail.com>
2023-03-19 23:08:39 +0600

    Listing 8-17. The TwoWayCounter Component

---
+++ Testing.ComponentTests/MouseTrackerShould.cs
@@ -0,0 +1,28 @@
+﻿using Bunit;
+using Microsoft.AspNetCore.Components.Web;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Testing.Client.Pages;
+
+namespace Testing.ComponentTests
+{
+    public class MouseTrackerShould : TestContext
+    {
+        [Fact]
+        public void ShowCorrectMousePosition()
+        {
+            var eventArgs = new MouseEventArgs()
+            {
+                ClientX = 100,
+                ClientY = 200
+            };
+            var cut = RenderComponent<MouseTracker>();
+            var theDiv = cut.Find(cssSelector: "div");
+            theDiv.MouseMove(eventArgs);
+            theDiv.MarkupMatches($"<div style:ignore>Mouse at {eventArgs.ClientX}x{eventArgs.ClientY}");
+        }
+    }
+}
