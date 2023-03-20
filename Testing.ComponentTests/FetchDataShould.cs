51998dce42d49a6591ca0ee23333c707a2f41b4b
Md. Masum Billah <md.masum123@gmail.com>
2023-03-21 02:17:58 +0600

    Listing 8-44. Testing the FetchData Component Using a Mock
    Listing 8-44. Testing the FetchData Component Using a Mock

--- Testing.ComponentTests/FetchDataShould.cs
+++ Testing.ComponentTests/FetchDataShould.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using Testing.Shared;
 using Testing.Client.Pages;
 using Xunit;
+using Microsoft.Extensions.Logging;
 
 namespace Testing.ComponentTests
 {
@@ -31,6 +32,26 @@ namespace Testing.ComponentTests
             var rows = cut.FindAll("tbody tr");
             rows.Count.Should().Be(nrOfForecasts);
         }
+
+        [Fact]
+        public void UseProperLogging()
+        {
+            const int nrOfForecasts = 5;
+            var stub = new WeatherServiceStub
+            {
+                FakeForecasts = Enumerable.Repeat(new WeatherForecast(),
+            nrOfForecasts)
+            };
+            Services.AddSingleton<IWeatherService>(stub);
+            LoggerMock logger = new LoggerMock();
+            Services.AddSingleton<ILogger>(logger);
+            var cut = RenderComponent<FetchData>();
+            logger.Journal.Count.Should().Be(1);
+            logger.Journal.First().state.Should().NotBeNull();
+            logger.Journal.First().state!.ToString().Should().Contain("Fetchingforecasts");
+        }
+
+
 
 
         //[Fact]
---
+++ Testing.ComponentTests/LoggerMock.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testing.ComponentTests
+{
+    public class LoggerMock : ILogger
+    {
+        public List<(LogLevel logLevel, object? state)> Journal
+        { get; set; } = new List<(LogLevel, object?)>();
+        public IDisposable BeginScope<TState>(TState state)
+        => throw new NotImplementedException();
+        public bool IsEnabled(LogLevel logLevel)
+        => true;
+        public void Log<TState>(LogLevel logLevel, EventId eventId,
+        TState state, Exception? exception,
+        Func<TState, Exception?, string> formatter)
+        {
+            Journal.Add((logLevel, state));
+        }
+    }
+}
