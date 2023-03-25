1c4aa1d0ae541ee493e8076de253a1c9a0a1f26a
Md. Masum Billah <md.masum123@gmail.com>
2023-03-25 17:21:59 +0600

    Listing 9-24. Showing a Navigating UI

--- Blazor.Communication/Client/Program.cs
+++ Blazor.Communication/Client/Program.cs
@@ -1,11 +1,12 @@
 using Blazor.Communication.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.AspNetCore.Components.WebAssembly.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
+builder.Services.AddScoped<LazyAssemblyLoader>();
 await builder.Build().RunAsync();
