9467ed001c1503654976db2498058df0db2e746b
Md. Masum Billah <md.masum123@gmail.com>
2023-03-28 23:58:29 +0600

    Listing 10-23. The Counter Component Using the JavaScript Module

--- JSInterop/Client/Services/ILocalStorage.cs
+++ JSInterop/Client/Services/ILocalStorage.cs
@@ -2,6 +2,7 @@
 {
     public interface ILocalStorage
     {
+        ValueTask Init();
         ValueTask<T> GetProperty<T>(string propName);
         ValueTask SetProperty<T>(string propName, T value);
         ValueTask WatchAsync<T>(T instance) where T : class;
--- JSInterop/Client/Services/LocalStorageWithModule.cs
+++ JSInterop/Client/Services/LocalStorageWithModule.cs
@@ -2,10 +2,11 @@
 
 namespace JSInterop.Client.Services
 {
-    public class LocalStorage : ILocalStorage
+    public class LocalStorageWithModule : ILocalStorage
     {
         private readonly IJSRuntime js;
-        public LocalStorage(IJSRuntime js)
+        private IJSObjectReference? module;
+        public LocalStorageWithModule(IJSRuntime js)
         {
             this.js = js;
         }
@@ -13,29 +14,24 @@ namespace JSInterop.Client.Services
         public ValueTask<T> GetProperty<T>(string propName)
         => js.InvokeAsync<T>("blazorLocalStorage.get", propName);
 
-        public ValueTask Init() => new ValueTask();
+        public async ValueTask Init()
+        {
+            module = module ?? await js.InvokeAsync<IJSObjectReference>
+            ("import", "./scripts/localstorage.js");
+        }
+        //public ValueTask Init() => new ValueTask();
 
 
-        //public ValueTask<T> GetProperty<T>(string propName)
-        //{
-        //    throw new NotImplementedException();
-        //}
+
 
         public ValueTask SetProperty<T>(string propName, T value)
         => js.InvokeVoidAsync("blazorLocalStorage.set", propName, value);
-        //public ValueTask SetProperty<T>(string propName, T value)
-        //{
-        //    throw new NotImplementedException();
-        //}
 
 
         public ValueTask WatchAsync<T>(T instance) where T : class
         => js.InvokeVoidAsync("blazorLocalStorage.watch",
         DotNetObjectReference.Create(instance));
 
-        //public ValueTask WatchAsync<T>(T instance) where T : class
-        //{
-        //    throw new NotImplementedException();
-        //}
+
     }
 }
