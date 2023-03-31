998e872cf45e1ce94304a073f992405f198c8dc7
Md. Masum Billah <md.masum123@gmail.com>
2023-04-01 01:48:02 +0600

    Listing 10-35. Passing the Markers to the Map Component

--- Leaflet.Map/wwwroot/map.js
+++ Leaflet.Map/wwwroot/map.js
@@ -6,18 +6,67 @@
     // Initialize map if needed
     if (!elem.map) {
         elem.map = L.map(elementId).setView([50.88022, 4.29660], zoom);
-        L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{ y } ? access_token = pk.eyJ1IjoibWFzdW1iZCIsImEiOiJjbGZzbHdrMmEwN2IxM2dxa3p2bHdybHo4In0.NhUB54ExeHAaPf7EV52XaA', {
-attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/
-copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.
-            mapbox.com / ">Mapbox</a>',
-maxZoom: 18,
+        elem.map.addedMarkers = [];
+        L.tileLayer('https://api.mapbox.com/styles/v1/masumbd/tiles/0/0/0 ? access_token = sk.eyJ1IjoibWFzdW1iZCIsImEiOiJjbGZ3eHB1dnUwN2RyM2VwaDU4eDUzaTN2In0._YJBce7 - p6y4ejVQz0xF2A', 
+        {attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com / ">Mapbox</a>',maxZoom: 18,
             id: 'mapbox/streets-v11',
             tileSize: 512,
             zoomOffset: -1,
-            accessToken: 'pk.eyJ1IjoibWFzdW1iZCIsImEiOiJjbGZzbHdrMmEwN2IxM2dxa3p2bHdybHo4In0.NhUB54ExeHAaPf7EV52XaA'
+            accessToken: 'sk.eyJ1IjoibWFzdW1iZCIsImEiOiJjbGZ3eHB1dnUwN2RyM2VwaDU4eDUzaTN2In0._YJBce7-p6y4ejVQz0xF2A'
 }).addTo(elem.map);
 }
-    export { showOrUpdate };
+// Add markers
+let map = elem.map;
+if (map.addedMarkers.length !== markers.length) {
+    // Markers have changed, so reset
+    map.addedMarkers.forEach(marker => marker.removeFrom(map));
+    map.addedMarkers = markers.map(m => {
+        return L.marker([m.y, m.x]).bindPopup(m.description).addTo(map);
+    });
+    // Auto-fit the view
+    var markersGroup = new L.featureGroup(map.addedMarkers);
+    map.fitBounds(markersGroup.getBounds().pad(0.3));
+    // Show applicable popups. Can't do this until after the view was auto - fitted.
+        markers.forEach((marker, index) => {
+            if (marker.showPopup) {
+                map.addedMarkers[index].openPopup();
+            }
+        });
+} else {
+// Same number of markers, so update positions/text without changing view bounds
+    markers.forEach((marker, index) => {
+        animateMarkerMove(
+            map.addedMarkers[index].setPopupContent(marker.description),marker,4000);
+    });
+}
+};
+let animateMarkerMove = (marker, coords, durationMs) => {
+    if (marker.existingAnimation) {
+        cancelAnimationFrame(marker.existingAnimation.callbackHandle);
+    }
+    marker.existingAnimation = {
+        startTime: new Date(),
+        durationMs: durationMs,
+        startCoords: { x: marker.getLatLng().lng, y: marker.getLatLng().lat },
+        endCoords: coords,
+        callbackHandle: window.requestAnimationFrame(() => animateMarkerMoveFrame(marker))
+    };
+}
+let animateMarkerMoveFrame = (marker) => {
+    var anim = marker.existingAnimation;
+    var proportionCompleted = (new Date().valueOf() - anim.startTime.valueOf()) / anim.durationMs;
+    var coordsNow = {
+        x: anim.startCoords.x + (anim.endCoords.x - anim.startCoords.x) * proportionCompleted,
+        y: anim.startCoords.y + (anim.endCoords.y - anim.startCoords.y) * proportionCompleted
+    };
+    marker.setLatLng([coordsNow.y, coordsNow.x]);
+    if (proportionCompleted < 1) {
+        marker.existingAnimation.callbackHandle = window.requestAnimationFrame(() => animateMarkerMoveFrame(marker));
+    }
+}
+
+export { showOrUpdate };
+
 
         ////https://account.mapbox.com/access-tokens
         //mapB_321#
