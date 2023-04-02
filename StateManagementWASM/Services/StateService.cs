e55d34c43bb4784536fcae873d41a97e4c40f3ce
Md. Masum Billah <md.masum123@gmail.com>
2023-04-02 20:39:09 +0600

    Listing 11-8. The Client’s StateService Class

---
+++ StateManagementWASM/Services/StateService.cs
@@ -0,0 +1,16 @@
+﻿using System.Net.Http.Json;
+
+namespace StateManagementWASM.Services
+{
+    public class StateService<T>
+    {
+        private readonly HttpClient httpClient;
+        public StateService(HttpClient httpClient)
+        {
+            this.httpClient = httpClient;
+        }
+
+        public async Task<int?> GetState(string user)=> await httpClient.GetFromJsonAsync<int>($"state/{user}");
+        public async Task SetState(string user, T state)=> await httpClient.PostAsJsonAsync($"state/{user}", state);
+    }
+}
