d617de65d21f3d296ed5589a04030231cf7b368e
Md. Masum Billah <md.masum123@gmail.com>
2023-04-03 01:25:18 +0600

    Listing 11-19. The Counter’s Markup

---
+++ UsingRedux/Client/Pages/Counter.razor.cs
@@ -0,0 +1,10 @@
+﻿using Fluxor;
+using Microsoft.AspNetCore.Components;
+using UsingRedux.Shared.Stores;
+
+public partial class Counter
+{
+    [Inject]
+    public IState<AppStore> State { get; set; } = default!;
+    public AppStore AppStore => State.Value;
+}
--- UsingRedux/Client/Program.cs
+++ UsingRedux/Client/Program.cs
@@ -1,11 +1,19 @@
+using Fluxor;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using System.Reflection;
 using UsingRedux.Client;
+using UsingRedux.Shared.Stores;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+Assembly storeAssembly = typeof(AppStore).Assembly;
+builder.Services
+.AddFluxor(options =>
+options.ScanAssemblies(storeAssembly));
 
 await builder.Build().RunAsync();
