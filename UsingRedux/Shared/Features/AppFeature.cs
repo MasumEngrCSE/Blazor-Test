6b9de5a13c7d88d44fd19c92b1717653cbb780fa
Md. Masum Billah <md.masum123@gmail.com>
2023-04-03 00:44:30 +0600

    Listing 11-14. The AppFeature Class

---
+++ UsingRedux/Shared/Features/AppFeature.cs
@@ -0,0 +1,22 @@
+﻿using Fluxor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UsingRedux.Shared.Stores;
+
+namespace UsingRedux.Shared.Features
+{
+    public class AppFeature : Feature<AppStore>
+    {
+        public override string GetName() => nameof(AppStore);
+
+        protected override AppStore GetInitialState()
+            => new AppStore(
+                            ClickCounter: 0,
+                            IsLoading: false,
+                            Forecasts: Array.Empty<WeatherForecast>()
+                            );
+    }
+}
