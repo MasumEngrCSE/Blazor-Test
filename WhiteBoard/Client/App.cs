fb7419c017b705faf2db1f057d5b59a5e35b9129
Md. Masum Billah <md.masum123@gmail.com>
2023-04-04 22:53:17 +0600

    Figure 12-2. The WhiteBoard Application in Action

---
+++ WhiteBoard/Client/App.cs
@@ -0,0 +1,22 @@
+﻿using WhiteBoard.Shared;
+
+namespace WhiteBoard.Client
+{
+    public partial class App
+    {
+        private readonly List<LineSegment> LineSegments= new List<LineSegment>();
+
+        private Task AddLineSegment(LineSegment segment)
+        {
+            List<LineSegment> segments =
+            new List<LineSegment>() { segment };
+            this.LineSegments.Add(segment);
+            return Task.CompletedTask;
+        }
+
+
+
+
+
+    }
+}
---
+++ WhiteBoard/Shared/LineSegment.cs
@@ -0,0 +1,20 @@
+﻿//using System;
+//using System.Collections.Generic;
+using System.Drawing;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+namespace WhiteBoard.Shared
+{
+    public struct LineSegment
+    {
+        public LineSegment(PointF start, PointF end)
+        {
+            Start = start;
+            End = end;
+        }
+        public PointF Start { get; set; }
+        public PointF End { get; set; }
+    }
+}
