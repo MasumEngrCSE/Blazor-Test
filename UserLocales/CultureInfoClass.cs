242d05e52d4b035df2491d253879cff31678c91a
Md. Masum Billah <md.masum123@gmail.com>
2023-04-18 01:19:33 +0600

    Listing 14-1. Using CultureInfo

---
+++ UserLocales/CultureInfoClass.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserLocales
+{
+    public class CultureInfoClass
+    {
+        public CultureInfoClass()
+        {
+
+        }
+
+
+        public void CultureInfoInstances()
+        {
+            var enUS = new CultureInfo("en-US");
+            Console.WriteLine(enUS.DateTimeFormat.GetDayName(DayOfWeek.Monday));
+            Console.WriteLine(enUS.NumberFormat.NumberDecimalSeparator);
+
+
+            var nlBE = new CultureInfo("nl-BE");
+            Console.WriteLine(nlBE.DateTimeFormat.GetDayName(DayOfWeek.Monday));
+            Console.WriteLine(nlBE.NumberFormat.NumberDecimalSeparator);
+        }
+    }
+}
