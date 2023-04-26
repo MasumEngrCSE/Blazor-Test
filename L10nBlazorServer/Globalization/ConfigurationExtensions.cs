86e351a07f2f285e5459769b47abce36c9c1a65f
Md. Masum Billah <md.masum123@gmail.com>
2023-04-26 10:13:34 +0600

    Listing 14-5. The ConfigurationExtensions Class

---
+++ L10nBlazorServer/Globalization/ConfigurationExtensions.cs
@@ -0,0 +1,11 @@
+﻿namespace L10nBlazorServer.Globalization
+{
+    public static class ConfigurationExtensions
+    {
+        public static Dictionary<string, string> GetCulturesSection
+                    (this IConfiguration configuration)
+                    => configuration.GetSection("Cultures")
+                    .GetChildren()
+                    .ToDictionary(k => k.Key, v => v.Value);
+    }
+}
