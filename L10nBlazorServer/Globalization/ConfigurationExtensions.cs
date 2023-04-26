187e2b5fa61f6535074b7d00c8cb10796d664d73
Md. Masum Billah <md.masum123@gmail.com>
2023-04-27 00:59:34 +0600

    Chapter 14 Supporting Multiple Languages in Your Blazor Applicati on

--- L10nBlazorServer/Globalization/ConfigurationExtensions.cs
+++ L10nBlazorServer/Globalization/ConfigurationExtensions.cs
@@ -1,22 +1,19 @@
-﻿using Microsoft.AspNetCore.Localization;
-using System.Configuration;
+﻿//using Microsoft.AspNetCore.Localization;
+//using System.Configuration;
 
+using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+using System.Linq;
+//L10nBlazorServer.Globalization
 namespace L10nBlazorServer.Globalization
 {
     public static class ConfigurationExtensions
     {
         public static Dictionary<string, string> GetCulturesSection
-                    (this IConfiguration configuration)
-                    => configuration.GetSection("Cultures")
-                    .GetChildren()
-                    .ToDictionary(k => k.Key, v => v.Value);
-
-
-
-
-
-
-
+                    (this IConfiguration configuration) => configuration
+            .GetSection("Cultures")
+            .GetChildren()
+            .ToDictionary(k => k.Key, v => v.Value);
 
 
     }
--- L10nBlazorServer/Program.cs
+++ L10nBlazorServer/Program.cs
@@ -1,6 +1,12 @@
 using L10nBlazorServer.Data;
+using L10nBlazorServer.Globalization;
+//using L10nBlazorServer.Globalization;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc.Razor;
+using System.Configuration;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +14,41 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
+
+//builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
+
+builder.Services.AddLocalization();
+builder.Services.AddControllersWithViews()
+    .AddViewLocalization
+    (LanguageViewLocationExpanderFormat.SubFolder)
+    .AddDataAnnotationsLocalization();
+
+builder.Services.Configure<RequestLocalizationOptions>(options => {
+
+    //Dictionary<string, string> cultures =Configuration.GetCulturesSection();
+    Dictionary<string, string> cultures = builder.Configuration.GetCulturesSection();
+    string[] supportedCultures =cultures.Keys.ToArray();
+
+    //var supportedCultures = new[] { "en-US", "fr", "de" };
+
+
+
+options.SetDefaultCulture(supportedCultures[0])
+    .AddSupportedCultures(supportedCultures)
+    .AddSupportedUICultures(supportedCultures);
+});
+
+Dictionary<string, string> cultures = builder.Configuration.GetCulturesSection();
+string[] supportedCultures = cultures.Keys.ToArray();
+
+var localizationOptions = new RequestLocalizationOptions()
+    .SetDefaultCulture(supportedCultures[0])
+    .AddSupportedCultures(supportedCultures)
+    .AddSupportedUICultures(supportedCultures);
+
+localizationOptions.RequestCultureProviders.Clear();
+localizationOptions.RequestCultureProviders.Add(new AcceptLanguageHeaderRequestCultureProvider());
+
 
 var app = builder.Build();
 
@@ -22,7 +63,7 @@ if (!app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseStaticFiles();
-
+app.UseRequestLocalization(localizationOptions);
 app.UseRouting();
 
 app.MapBlazorHub();
