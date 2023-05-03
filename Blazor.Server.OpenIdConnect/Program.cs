b52428cadaded8755f83b9c5e2a4b5c0ad28586d
Md. Masum Billah <md.masum123@gmail.com>
2023-05-04 01:23:55 +0600

    Listing 16-20. Add the Login.cshtml Razor Page

--- Blazor.Server.OpenIdConnect/Program.cs
+++ Blazor.Server.OpenIdConnect/Program.cs
@@ -1,4 +1,6 @@
 using Blazor.Server.OpenIdConnect.Data;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 
@@ -8,6 +10,32 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultScheme =
+    CookieAuthenticationDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme =
+    OpenIdConnectDefaults.AuthenticationScheme;
+})
+.AddCookie(CookieAuthenticationDefaults.AuthenticationScheme)
+.AddOpenIdConnect(OpenIdConnectDefaults.AuthenticationScheme,
+options =>
+{
+    options.SignInScheme =
+    CookieAuthenticationDefaults.AuthenticationScheme;
+    options.Authority = "https://localhost:5011";
+    options.ClientId = "BlazorServer";
+    options.ClientSecret = "u2u-secret";
+    // When set to code, the middleware will use PKCE protection
+    options.ResponseType = "code id_token";
+    // It's recommended to always get claims from the
+    // UserInfoEndpoint during the flow.
+    options.GetClaimsFromUserInfoEndpoint = true;
+});
+
+
+
 
 var app = builder.Build();
 
@@ -23,6 +51,8 @@ app.UseHttpsRedirection();
 
 app.UseStaticFiles();
 
+app.UseAuthentication();
+app.UseAuthorization();
 app.UseRouting();
 
 app.MapBlazorHub();
